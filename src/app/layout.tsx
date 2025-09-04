5399782ce55f85403c84ebc85f945c82541d6ef9
Lorenzo <lorenzo.dastoli@gmail.com>
2025-09-04 15:00:28 +0200

    updated the postcss.config file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Jaldi, Jersey_10 } from "next/font/google";
+
 import "./globals.css";
 import Header from "../app/components/server/Header";
 import Footer from "../app/components/server/Footer";
